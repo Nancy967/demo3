f3fe1785db90ed64bceb106a1304b9c88ef1f75f
Nancy967 <beg7520@gmail.com>
2022-07-11 19:05:44 +0800

    CrudRepository

--- src/main/java/com/example/demo/StudentController.java
+++ src/main/java/com/example/demo/StudentController.java
@@ -1,9 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class StudentController {
@@ -17,5 +15,40 @@ public class StudentController {
         studentRepository.save(student);
 
         return "Create";
+    }
+
+    @PutMapping("/students/{studentId}")
+    public String update(@PathVariable Integer studentId,
+                         @RequestBody Student student){
+
+        Student s = studentRepository.findById(studentId).orElse(null);
+        if (s!=null){
+            s.setName(student.getName());
+            studentRepository.save(s);
+            return "Update";
+        }else {
+            return "數據不存在，無法更新";
+        }
+
+        /*student.setId(studentId);
+        studentRepository.save(student);
+
+        return "Update";*/
+    }
+
+    @DeleteMapping("/students/{studentId}")
+    public String delete(@PathVariable Integer studentId){
+
+        studentRepository.deleteById(studentId);
+
+        return "Delete";
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student read(@PathVariable Integer studentId){
+
+        Student student = studentRepository.findById(studentId).orElse(null);
+
+        return student;
     }
 }
